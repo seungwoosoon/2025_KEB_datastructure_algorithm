f17e35d82a0b8875736a693fbdb8c862a63b98e2
seuntwoosoon <tnddm0927@naver.com>
2025-02-17 17:28:37 +0900

    homework

--- day06.py
+++ day06.py
@@ -1,4 +1,5 @@
 import time
+# import numpy as np
 def timer(func):
     def wrapper(*args, **kwargs):
         start = time.time()
@@ -38,9 +39,50 @@ def fibo(n) -> int:
     else:
         memo[n] = fibo(n-2) + fibo(n-1)
         return memo[n]
+    
+factmemo = {}
+def fact(n) -> int:
+    if n in factmemo:
+        return factmemo[n]
+    elif n <= 1:
+        return n
+    else:
+        factmemo[n] = fact(n-1) * n
+        return factmemo[n]
 
 # n = int(input())
 # print(is_even(n))
 # print(eightjinsu(65))
-print(fibo(5))
-print(memo)
+# print(fibo(5))
+# print(fact(10))
+# print(memo)
+
+import random
+ran = [1,100]
+def guessnum(f,l):
+    # return random.randint(f,l)
+    return int((f+l)/2)
+
+answer = random.randint(1, 100)
+chance = 7
+
+with open('guess.txt','w') as fp:
+    while chance != 0:
+        guess = guessnum(ran[0],ran[1])
+        if guess == answer:
+            print(f'You win. Answer is {answer}')
+            fp.write(f"{answer}정답 \n")
+            break
+        elif guess > answer:
+            chance = chance - 1
+            print(f'{guess} is bigger. Chance left : {chance}')
+            fp.write(f"{guess} 커요 \n")
+            ran[1] = guess-1
+        else:
+            chance = chance - 1
+            print(f'{guess} is lower. Chance left : {chance}')
+            fp.write(f"{guess} 작아요 \n")
+            ran[0] = guess +1
+    else:
+        print(f'You lost. Answer is {answer}')
+        fp.write("실패~~~~ \n")
