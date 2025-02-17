ea757edf532fe0a8cb9b468a7025b7682bf0fee7
seuntwoosoon <tnddm0927@naver.com>
2025-02-17 14:55:32 +0900

    dynamicprogramming

--- day06.py
+++ day06.py
@@ -1,8 +1,19 @@
+import time
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} 실행 시간: {end - start:.9f}초")
+        return result
+    return wrapper
+
 def factorial(n):
     if n==1:
         return 1
     return n*factorial(n-1)
 
+@timer
 def eightjinsu(n):
     if n == 0:
         return ""
@@ -18,6 +29,18 @@ def is_even(n) -> bool:
     """
     return not bool(n&1)
 
+memo = dict()
+def fibo(n) -> int:
+    if n in memo:
+        return memo[n]
+    elif n<=1:
+        return n
+    else:
+        memo[n] = fibo(n-2) + fibo(n-1)
+        return memo[n]
+
 # n = int(input())
 # print(is_even(n))
-print(eightjinsu(65))
+# print(eightjinsu(65))
+print(fibo(5))
+print(memo)
