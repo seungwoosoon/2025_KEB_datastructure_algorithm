41c2b22c5785216306ea7af639fd022cf75ec504
seuntwoosoon <tnddm0927@naver.com>
2025-02-19 15:34:32 +0900

    dfs

---
+++ graph.py
@@ -0,0 +1,18 @@
+def dfs(g, v, visited):
+    visited[v] = True
+    print(chr(ord('A')+v), end=' ')
+    for i in range(len(g)):
+        if g[v][i] == True and not visited[i]:
+            dfs(g, i, visited)
+    return
+graph = [
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0]
+]
+
+visited = [False] * len(graph)
+dfs(graph, 0, visited)
